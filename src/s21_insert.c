b5292d8553e263b21cf9bb38f8181d0e768ce126
Mike Dawlish <mikedawl@oa-h5.msk.21-school.ru>
2023-12-14 03:35:24 +0300

    somewhat fixed insert

--- src/s21_insert.c
+++ src/s21_insert.c
@@ -1,13 +1,19 @@
 #include "s21_string.h"
 #include <string.h>
 void *s21_insert(const char *src, const char *str, size_t start_index){
-    size_t length = s21_strlen(src) + s21_strlen(str) + 1;
+    size_t length = strlen(src) + strlen(str) + 1;
     char* product = (char*)malloc(length);
-    s21_strncpy(product, src, start_index);
+    if (start_index> s21_strlen(src)){
+        return NULL;
+    }
+    strncpy(product, src, start_index);
     product[start_index] = '\0';
     strcat(product, str);
     strcat(product, src + start_index);
-    s21_strncpy((char *)src, product, length);
-    free(product);
+    strncpy((char *)src, product, length);
+    if (product){
+        free(product);
+    }
+    
     return (void *)src;
 }
