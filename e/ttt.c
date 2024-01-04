8d581de632b52d96efabdc87eee9489009865206
miklazru <miklazru@student.21-school.ru>
2024-01-04 03:39:16 +0300

    fff

---
+++ e/ttt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    const char *input = "   12345a123kfg";
+    int num;
+
+    int result = sscanf(input, "%8d", &num);
+
+    if (result == 1) {
+        printf("Число: %d\n", num);
+    } else {
+        printf("Не удалось интерпретировать число.\n");
+    }
+
+    return 0;
+}
